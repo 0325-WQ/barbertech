be348bf1ea7e16cb5f6c1ee9bf90ae017ce5fc8a
unknown <garciavergara401@gmail.com>
2026-06-28 15:12:46 -0500

    modif2

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 require('dotenv').config(); // Carga las variables directamente desde el .env de la carpeta backend
 
-const connection = mysql.createPool({
+const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'admin_barbertech',
     password: '2026Segura_Cont',
